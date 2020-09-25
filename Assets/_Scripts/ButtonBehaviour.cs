e45a0c4dd1df577dd7a4ade743ee5de7d9022d2d
EmmanuelN-L <46698046+EmmanuelN-L@users.noreply.github.com>
2020-09-25 19:05:35 -0400

    Added New Script for Buttons
    Each Button is now using the appropriate functions from the script

--- Assets/_Scripts/ButtonBehaviour.cs
+++ Assets/_Scripts/ButtonBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonBehaviour : MonoBehaviour
 {
@@ -14,5 +15,29 @@ public class ButtonBehaviour : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnPlayButtonPressed()
+    {
+        Debug.Log("Play Button Pressed");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnMainMenuButtonPressed()
+    {
+        Debug.Log("Main Menu Button Pressed");
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void OnGameOverButtonPressed()
+    {
+        Debug.Log("Game Over Button Pressed");
+        SceneManager.LoadScene("GameOverScene");
+    }
+
+    public void OnRestartButtonPressed()
+    {
+        Debug.Log("Restart Button Pressed");
+        SceneManager.LoadScene("GameScene");
     }
 }
