d6ae087438ac1218f544c92f065dcc488503728e
EmmanuelN-L <46698046+EmmanuelN-L@users.noreply.github.com>
2020-10-03 18:22:16 -0400

    Added Backgrounds, Sounds and Soundtracks
    Fixed UI to fit any screen resolution. Updated scenes to fit screen resolution correctly for Samsung S7.

---
+++ Assets/_Scripts/Shop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void PurchaseGreenTurret ()
+    {
+        Debug.Log("Green Turret Purchased");
+    }
+
+    public void PurchaseRedTurret()
+    {
+        Debug.Log("Red Turret Purchased");
+    }
+
+
+}
